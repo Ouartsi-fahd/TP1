d66f8cb9a6ec790c61faf8f15556645f6030804c
Fahd <ouartsifahd1940@gmail.com>
2024-03-19 22:12:55 +0100

    First Commit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CalculatorTest {
+
+    @Test
+    void add() {
+        assertEquals(5 , Calculator.add(2,3));
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+}
